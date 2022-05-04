72ce98e097f1d8b9d2e2fb56ba15aacd3046a3d2
yang <1173278084@qq.com>
2022-05-04 18:37:09 +0800

    创建项目基本架构，先优化目录

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
-<!--
- * @Author: 'yang' '1173278084@qq.com'
- * @Date: 2022-05-02 18:20:38
- * @LastEditors: 'yang' '1173278084@qq.com'
- * @LastEditTime: 2022-05-04 17:59:23
- * @FilePath: \mimall\src\App.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
 <template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-export default {
-  
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-</script>
 
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
