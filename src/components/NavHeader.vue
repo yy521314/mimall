a64e9a804c591b7a2d501e9057a60f59b8712262
yang <1173278084@qq.com>
2022-05-04 18:10:05 +0800

    创建项目基本架构，目录初始化

---
+++ src/components/NavHeader.vue
@@ -0,0 +1,23 @@
+<!--
+ * @Author: 'yang' '1173278084@qq.com'
+ * @Date: 2022-05-04 17:53:58
+ * @LastEditors: 'yang' '1173278084@qq.com'
+ * @LastEditTime: 2022-05-04 17:54:21
+ * @FilePath: \mimall\src\components\NavHeader.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+  <div>
+      Header
+  </div>
+</template>
+
+<script>
+export default {
+    name:'NavHeader'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/home.vue
@@ -0,0 +1,25 @@
+<!--
+ * @Author: 'yang' '1173278084@qq.com'
+ * @Date: 2022-05-02 18:20:38
+ * @LastEditors: 'yang' '1173278084@qq.com'
+ * @LastEditTime: 2022-05-04 17:48:16
+ * @FilePath: \mimall\src\views\Home.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+  <div class="home">
+    home
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
